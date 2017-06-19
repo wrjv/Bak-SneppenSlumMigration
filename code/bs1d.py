e3e61490393b87881f0f4e1bdfbcd91b56165c87
Willem Vermeulen <willem.vermeulen@planet.nl>
2017-06-19 10:30:00 +0200

    Added ages

--- code/bs1d.py
+++ code/bs1d.py
@@ -18,22 +18,30 @@ class BaxSneppen1D(object):
         while self.update_state():
             continue
 
+        print(self.ages[-1])
         print(len(self.states))
 
     def update_state(self):
+        self.ages.append(self.ages[-1] + 1)
+
         # Build a new state
         new_state = deepcopy(self.states[-1])
 
         min_val = np.argmin(new_state)
 
         # Stopping criterium
-        if new_state[min_val] > 0.65:
+        if new_state[min_val] > 0.60:
             return False
 
         # Modify the values around the minimum value
         new_state[min_val - 1] = np.random.uniform(0, 1, 1)
         new_state[min_val] = np.random.uniform(0, 1, 1)
         new_state[(min_val + 1) % len(new_state)] = np.random.uniform(0, 1, 1)
+
+        # Modify the cell ages
+        self.ages[-1][min_val - 1] = 0
+        self.ages[-1][min_val] = 0
+        self.ages[-1][(min_val + 1) % len(new_state)] = 0
 
         self.states.append(new_state)
 
