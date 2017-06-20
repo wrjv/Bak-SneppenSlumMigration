987143c1314edaa05f79713efa56fa2cfbe95cec
Wessel Klijnsma <wesselklijnsma@gmail.com>
2017-06-20 13:49:09 +0200

    grid animations

--- code/slumnetwork.py
+++ code/slumnetwork.py
@@ -1,14 +1,21 @@
 from bs2d import BaxSneppen2D
 import numpy as np
+import matplotlib.pyplot as plt
+from copy import deepcopy
+import matplotlib.animation as animation
 
 class Slums(object):
 
     def __init__(self, n_slums, slum_size=(15, 15)):
         self.slum_list = [BaxSneppen2D(slum_size) for _ in range(n_slums)]
+        self.states = list()
+        self.time = 0
 
     def execute(self, moore=False):
         while self.update_state(moore):
+            self.states.append(deepcopy(self.slum_list))
             continue
+        self.plot_slums()
 
     def update_state(self, moore=False):
         min_vals = [slum.get_min_val() for slum in self.slum_list]
@@ -18,9 +25,33 @@ class Slums(object):
         # TODO Modify this part to make some network structure
 
         self.slum_list[(np.argmin(min_vals) + 1) % len(self.slum_list)].add_to_grid(min(min_vals))
+        if self.time > 100:
+            return False
+
+        self.time += 1
+        return True
+
+    def plot_slums(self):
+        f, axarr = plt.subplots(4, sharex=True, sharey=True)
+        ims = list()
+        for slum, ax in zip(self.slum_list, axarr):
+            ims.append(ax.imshow(slum.state, aspect='auto', cmap='jet_r', interpolation='nearest'))
+
+        def animate(i):
+            plt.title('iteration: ' + str(i))
+            for slum, im, in zip(self.states[i], ims):
+                im.set_array(slum.state)
+            f.canvas.draw()
+            return ims
+
+        ani = animation.FuncAnimation(f, animate, range(int(len(self.states) * 0), len(self.states)), interval=2, blit=False)
+        plt.show()
+
+
+
 
 def main():
-    slums = Slums(4)
+    slums = Slums(4, (50,50))
     slums.execute()
 
 if __name__ == '__main__':
