50c159e354f5aa529d0db8ca55de84b39b318ab5
Maarten-vd-Sande <maartenvandersande@hotmail.com>
2017-06-21 15:49:55 +0200

    added a plot for the avalanche size, + general cleanup

--- code/slumnetwork.py
+++ code/slumnetwork.py
@@ -19,10 +19,16 @@ class Slums(object):
         self.time = 0
         self.previous_location = [(0,0) for _ in range(n_slums)]
         self.distances = [[] for _ in range(n_slums)]
+        self.avalanche_size = [0]
+        self.aval_start_val = 0
 
-    def execute(self, moore=False):
+    def execute(self, moore=False, save_steps=25):
+        iterator = 0
+        self.save_steps = save_steps
         while self.update_state(moore):
-            self.states.append(deepcopy(self.slum_list))
+            if iterator % save_steps == 0:
+                self.states.append(deepcopy(self.slum_list))
+            iterator += 1
             continue
 
     def update_state(self, moore=False):
@@ -35,6 +41,13 @@ class Slums(object):
         distance = ((x-xold)**2 + (y-yold)**2)**0.5
         self.distances[np.argmin(min_vals)].append(distance)
         self.previous_location[np.argmin(min_vals)] = (x,y)
+
+        # calculate the size of the avalanche
+        if min(min_vals) >= self.aval_start_val:
+            self.avalanche_size.append(1)
+            self.aval_start_val = min(min_vals)
+        else:
+            self.avalanche_size[-1] += 1
 
         for slum in self.slum_list:
             slum.update_ages()
@@ -82,7 +95,7 @@ class Slums(object):
 
         return np.random.choice(range(len(self.slum_list)), 1, p=pvalues)
 
-    def plot_slums(self, start, show_steps):
+    def plot_slums(self, start):
         cols = ceil(len(self.slum_list)**0.5)
         rows = ceil(len(self.slum_list)/cols)
 
@@ -111,21 +124,21 @@ class Slums(object):
             assert False
 
         def animate(i):
-            plt.suptitle('iteration: ' + str(i))
+            plt.suptitle('iteration: ' + str(i*self.save_steps))
             for slum, im, in zip(self.states[i], ims):
                 im.set_array(slum.ages)
             f.canvas.draw()
             return ims
 
         ani = animation.FuncAnimation(f, animate, range(int(len(self.states) * start),
-                                                        len(self.states), show_steps), interval=2, blit=False)
+                                                        len(self.states), 1), interval=2, blit=False)
         plt.show()
 
 
     def plot_barrier_distribution(self):
         barriers = []
         minima = []
-        for slum in self.states[-1]:
+        for slum in self.slum_list:
             barriers = barriers + list(slum.state[np.where(slum.state <= 1)].flatten())
         for timestep in self.states:
             minima.append(min([state.get_min_val() for state in timestep]))
@@ -156,13 +169,19 @@ class Slums(object):
         plt.hist(barriers, bins=30, range=(0, 1))
         plt.show()
 
+    def plot_avalanche_size(self):
+        (counts, bins, _) = plt.hist(self.avalanche_size, bins=30)
+        plt.clf()
+        plt.loglog(bins[:-1], counts/sum(counts))
+        plt.show()
 
 def main():
-    slums = Slums(9, (25, 25), empty_percent=0.06)
-    slums.execute()
+    slums = Slums(4, (30, 30), empty_percent=0.06)
+    slums.execute(save_steps=50)
     # slums.plot_barrier_distribution()
     # slums.plot_avalanche_distance()
-    slums.plot_slums(start=0, show_steps=25)
+    slums.plot_avalanche_size()
+    # slums.plot_slums(start=0)
 
 if __name__ == '__main__':
     main()
