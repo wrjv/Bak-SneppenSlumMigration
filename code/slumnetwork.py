0e497699d7006a9617d61749987d5e5fdb11ae7a
Wessel Klijnsma <wesselklijnsma@gmail.com>
2017-06-29 12:47:39 +0200

    network animation

--- code/slumnetwork.py
+++ code/slumnetwork.py
@@ -769,7 +769,7 @@ class Slums(object):
         plt.show()
 
     def plot_network(self):
-        figure = plt.figure()
+        figure = plt.figure(figsize=(14, 9))
 
         def animate(i):
             G = nx.from_numpy_matrix(self.migration_matrices[i][:-1, :-1], create_using=nx.DiGraph())
@@ -830,9 +830,10 @@ class Slums(object):
             nx.draw_networkx_edge_labels(G, pos=layout, edge_labels=edge_labels, label_pos=0.9, font_size=16,
                                          bbox=bbox_opts)
 
-        plt.savefig('../docs/videos/slum_network.png')
-        ani = animation.FuncAnimation(figure, animate, range(0, len(self.migration_matrices)), interval=400,
+
+        ani = animation.FuncAnimation(figure, animate, range(0, len(self.migration_matrices)), interval=80,
                                     blit=False)
+        plt.savefig('../docs/videos/slum_network.png')
         ani.save('../docs/videos/slum_network.gif', writer='imagemagick')
         plt.show()
 
